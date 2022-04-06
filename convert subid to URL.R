4bf8c2d6731857b1a4243e02e115dcfbe3e2ee71
Nick Anich <47158373+nmanich@users.noreply.github.com>
2022-04-06 09:17:47 -0500

    added snakecase version

--- convert subid to URL.R
+++ convert subid to URL.R
@@ -1,5 +1,6 @@
 #make URL link for Shiny table
 #turns ebird subid into a clickable html URL
+# NOTE: THE TOP HALF IS IF YOUR HEADERS ARE LIKE COMMON.NAME AND THE BOTTOM HALF IS IF YOUR HEADERS ARE LIKE common_name
 
 subidtable <- read.delim("ebd_US-WI_201501_201912_relDec-2020.txt", sep="\t", header=TRUE, quote = "", stringsAsFactors = FALSE, na.strings=c(""))
 
@@ -22,3 +23,38 @@ export <- subidunique[, c("SAMPLING.EVENT.IDENTIFIER","ChecklistLink")]
 
 #Then export:
 write.csv(export, file= "subids_with_url9.csv", row.names=FALSE)
+
+
+
+
+############################################################################################################################################################
+############################################################################################################################################################
+# THE SNAKECASE VERSION
+
+#make URL link for Shiny table
+#turns ebird subid into a clickable html URL
+
+subidtable <- read.delim("March2022dataset.txt", sep="\t", header=TRUE, quote = "", stringsAsFactors = FALSE, na.strings=c(""))
+
+#keep only unique subids
+subidunique <- subidtable[!duplicated(subidtable$sampling_event_identifier), ]
+
+# Add new columns 
+subidunique$front <- '<a href="https://ebird.org/checklist/'
+
+subidunique$middle <- '/" target="_blank">https://ebird.org/checklist/'
+
+subidunique$back <- '</a>'
+
+# concatenate into eBird URL
+subidunique$ChecklistLink <-  with(subidunique, paste0(front, sampling_event_identifier, middle, sampling_event_identifier, back))
+
+#Select columns for export:
+#Select all columns and only the rows you want:
+export <- subidunique[, c("sampling_event_identifier","ChecklistLink")]
+
+#Then export:
+write.csv(export, file= "subids_with_url.csv", row.names=FALSE)
+
+
+
